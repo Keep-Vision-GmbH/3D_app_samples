8dda8fcd88f652912c5f71c886e664ceaa31a8a0
Lutzel <56841302+Lutzel@users.noreply.github.com>
2020-12-06 12:42:15 +0100

    index_change

--- quicklook_sample_2/index_separator_js.js
+++ quicklook_sample_2/index_separator_js.js
@@ -2,21 +2,22 @@
 function detectBrowser() {
     if ((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1) {
         //return 'Opera';
-        open("index_android.html"); 
+        open("index_android.html");
     } else if (navigator.userAgent.indexOf("Chrome") != -1) {
         //return 'Chrome';
-        open("index_android.html"); 
+        open("index_android.html");
     } else if (navigator.userAgent.indexOf("Safari") != -1) {
         //return 'Safari';
-        open("index_apple.html"); 
+        open("index_apple.html");
     } else if (navigator.userAgent.indexOf("Firefox") != -1) {
         //return 'Firefox';
-        open("index_android.html"); 
+        open("index_android.html");
     } else if ((navigator.userAgent.indexOf("MSIE") != -1) || (!!document.documentMode == true)) {
         //return 'IE';
-        open("index_android.html"); 
+        open("index_android.html");
     } else {
         //return 'Unknown';
-        open("index_android.html"); 
+        open("index_android.html");
     }
+
 } 
