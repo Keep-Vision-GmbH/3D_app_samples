9bf5b41f05fe1ac5cbfe6fd87a429387f9f100af
Lutzel <56841302+Lutzel@users.noreply.github.com>
2020-12-06 19:01:54 +0100

    ar update

--- sub_1/app.js
+++ sub_1/app.js
@@ -1,72 +1,134 @@
 import * as THREE from '../../libs/three/three.module.js';
+import { GLTFLoader } from '../../libs/three/jsm/GLTFLoader.js';
+import { FBXLoader } from '../../libs/three/jsm/FBXLoader.js';
+import { RGBELoader } from '../../libs/three/jsm/RGBELoader.js';
 import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
-import { Stats } from '../../libs/stats.module.js';
-import { ARButton } from '../../libs/ARButton.js';
+import { LoadingBar } from '../../libs/LoadingBar.js';
 
 class App{
 	constructor(){
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
         
-        this.clock = new THREE.Clock();
+		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 100 );
+		this.camera.position.set( 0, 4, 14 );
         
-		this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
-		
 		this.scene = new THREE.Scene();
-       
-		this.scene.add( new THREE.HemisphereLight( 0x606060, 0x404040 ) );
-
-        const light = new THREE.DirectionalLight( 0xffffff );
-        light.position.set( 1, 1, 1 ).normalize();
-		this.scene.add( light );
+        this.scene.background = new THREE.Color( 0xaaaaaa );
+        
+		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.5);
+		this.scene.add(ambient);
+        
+        const light = new THREE.DirectionalLight( 0xFFFFFF, 1.5 );
+        light.position.set( 0.2, 1, 1);
+        this.scene.add(light);
 			
 		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
 		this.renderer.setPixelRatio( window.devicePixelRatio );
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
         this.renderer.outputEncoding = THREE.sRGBEncoding;
+        this.renderer.physicallyCorrectLights = true;
+        container.appendChild( this.renderer.domElement );
+		this.setEnvironment();
+		
+        this.loadingBar = new LoadingBar();
         
-		container.appendChild( this.renderer.domElement );
+        this.loadGLTF();
+        //this.loadFBX();
         
         this.controls = new OrbitControls( this.camera, this.renderer.domElement );
         this.controls.target.set(0, 3.5, 0);
         this.controls.update();
         
-        this.stats = new Stats();
-        
-        this.initScene();
-        this.setupVR();
-        
         window.addEventListener('resize', this.resize.bind(this) );
 	}	
     
-    initScene(){
-        this.geometry = new THREE.BoxBufferGeometry( 0.06, 0.06, 0.06 ); 
-        this.meshes = [];
-    }
-    
-    setupVR(){
-        this.renderer.xr.enabled = true; 
+    setEnvironment(){
+        const loader = new RGBELoader().setDataType( THREE.UnsignedByteType );
+        const pmremGenerator = new THREE.PMREMGenerator( this.renderer );
+        pmremGenerator.compileEquirectangularShader();
         
         const self = this;
-        let controller;
         
-        function onSelect() {
-            const material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random() } );
-            const mesh = new THREE.Mesh( self.geometry, material );
-            mesh.position.set( 0, 0, - 0.3 ).applyMatrix4( controller.matrixWorld );
-            mesh.quaternion.setFromRotationMatrix( controller.matrixWorld );
-            self.scene.add( mesh );
-            self.meshes.push( mesh );
+        loader.load( '../../assets/hdr/venice_sunset_1k.hdr', ( texture ) => {
+          const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
+          pmremGenerator.dispose();
 
-        }
+          self.scene.environment = envMap;
 
-        const btn = new ARButton( this.renderer );
-        
-        controller = this.renderer.xr.getController( 0 );
-        controller.addEventListener( 'select', onSelect );
-        this.scene.add( controller );
-        
-        this.renderer.setAnimationLoop( this.render.bind(this) );
+        }, undefined, (err)=>{
+            console.error( 'An error occurred setting the environment');
+        } );
+    }
+    
+    loadGLTF(){
+        const loader = new GLTFLoader( ).setPath('../../assets/');
+        const self = this;
+		
+		// Load a glTF resource
+		loader.load(
+			// resource URL
+			'office-chair.glb',
+			// called when the resource is loaded
+			function ( gltf ) {
+                const bbox = new THREE.Box3().setFromObject( gltf.scene );
+                console.log(`min:${bbox.min.x.toFixed(2)},${bbox.min.y.toFixed(2)},${bbox.min.z.toFixed(2)} -  max:${bbox.max.x.toFixed(2)},${bbox.max.y.toFixed(2)},${bbox.max.z.toFixed(2)}`);
+                
+                gltf.scene.traverse( ( child ) => {
+                    if (child.isMesh){
+                        child.material.metalness = 0.2;
+                    }
+                })
+                self.chair = gltf.scene;
+                
+				self.scene.add( gltf.scene );
+                
+                self.loadingBar.visible = false;
+				
+				self.renderer.setAnimationLoop( self.render.bind(self));
+			},
+			// called while loading is progressing
+			function ( xhr ) {
+
+				self.loadingBar.progress = (xhr.loaded / xhr.total);
+				
+			},
+			// called when loading has errors
+			function ( error ) {
+
+				console.log( 'An error happened' );
+
+			}  
+        );
+    }
+    
+    loadFBX(){
+        const loader = new FBXLoader( ).setPath('../../assets/');
+        const self = this;
+    
+        loader.load( 'office-chair.fbx', 
+            function ( object ) {    
+                self.chair = object;
+
+                self.scene.add( object );
+            
+                self.loadingBar.visible = false;
+            
+                self.renderer.setAnimationLoop( self.render.bind(self));
+            },
+			// called while loading is progressing
+			function ( xhr ) {
+
+				self.loadingBar.progress = (xhr.loaded / xhr.total);
+				
+			},
+			// called when loading has errors
+			function ( error ) {
+
+				console.log( 'An error happened' );
+
+			} 
+        );
     }
     
     resize(){
@@ -76,8 +138,7 @@ class App{
     }
     
 	render( ) {   
-        this.stats.update();
-        this.meshes.forEach( (mesh) => { mesh.rotateY( 0.01 ); });
+        this.chair.rotateY( 0.01 );
         this.renderer.render( this.scene, this.camera );
     }
 }
